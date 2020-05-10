480a5397145bb529121ea8985379474f7890ca7d
GWCMelody <melody2011102@gmail.com>
2020-05-09 20:24:31 -0700

    Added main and tracks js files

--- 2020/parts/main.js
+++ 2020/parts/main.js
@@ -13,3 +13,64 @@ function changeVisibility() {
         info.style.maxHeight = info.scrollHeight + "px";
     }
 }
+
+//For About
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//for tracks
+$(document).ready(() => {
+    let $curwindow;
+  
+    $('#track1').on('click', () => {
+        $('.black-overlay').fadeIn('fast');
+        $('#track1-info').fadeIn('medium');
+        $curwindow = $('#track1-info');
+    });
+  
+    $('#track2').on('click', () => {
+        $('.black-overlay').fadeIn('fast');
+        $('#track2-info').fadeIn('medium');
+        $curwindow = $('#track2-info');
+    });
+  
+    $('.close').on('click', () => {
+        $('.black-overlay').fadeOut('fast');
+        $curwindow.fadeOut('fast');
+    });
+  
+    $('#track3').on('click', () => {
+        $('.black-overlay').fadeIn('fast');
+        $('#track3-info').fadeIn('medium');
+        $curwindow = $('#track3-info');
+    });
+  
+    $('.black-overlay').on('click', () => {
+        $('.black-overlay').fadeOut('fast');
+        $curwindow.fadeOut('fast');
+    });
+  });
