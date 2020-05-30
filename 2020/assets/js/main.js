37eb5dadc876eb9487adae8312027b30b233a993
GWCMelody <melody2011102@gmail.com>
2020-05-29 21:02:43 -0700

    fixed margins

--- 2020/assets/js/main.js
+++ 2020/assets/js/main.js
@@ -2,16 +2,16 @@ var FAQs = document.getElementsByClassName("FAQButton");
 var i;
 
 for (i = 0; i < FAQs.length; i++) {
-    FAQs[i].addEventListener("click", changeVisibility);
+  FAQs[i].addEventListener("click", changeVisibility);
 }
 
 function changeVisibility() {
-    var info = this.nextElementSibling;
-    if (info.style.maxHeight) {
-        info.style.maxHeight = null;
-    } else {
-        info.style.maxHeight = info.scrollHeight + "px";
-    }
+  var info = this.nextElementSibling;
+  if (info.style.maxHeight) {
+    info.style.maxHeight = null;
+  } else {
+    info.style.maxHeight = info.scrollHeight + "px";
+  }
 }
 
 //For About
@@ -30,47 +30,63 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 //for tracks
 $(document).ready(() => {
-    let $curwindow;
-  
-    $('#track1').on('click', () => {
-        $('.black-overlay').fadeIn('fast');
-        $('#track1-info').fadeIn('medium');
-        $curwindow = $('#track1-info');
-    });
-  
-    $('#track2').on('click', () => {
-        $('.black-overlay').fadeIn('fast');
-        $('#track2-info').fadeIn('medium');
-        $curwindow = $('#track2-info');
-    });
-  
-    $('.close').on('click', () => {
-        $('.black-overlay').fadeOut('fast');
-        $curwindow.fadeOut('fast');
-    });
-  
-    $('#track3').on('click', () => {
-        $('.black-overlay').fadeIn('fast');
-        $('#track3-info').fadeIn('medium');
-        $curwindow = $('#track3-info');
-    });
-  
-    $('.black-overlay').on('click', () => {
-        $('.black-overlay').fadeOut('fast');
-        $curwindow.fadeOut('fast');
-    });
+  let $curwindow;
+
+  $('#top').on('click', () => {
+    $('#header').goTo();
   });
+  $('#applyAnchor').on('click', () => {
+    $('#logistics').goTo();
+  });
+  $('#tracksAnchor').on('click', () => {
+    $('#challenges').goTo();
+  });
+  $('#logisticsAnchor').on('click', () => {
+    $('#applySection').goTo();
+  });
+  $('#aboutAnchor').on('click', () => {
+    $('#aboutSection').goTo();
+  });
+
+  $('#track1').on('click', () => {
+    $('.black-overlay').fadeIn('fast');
+    $('#track1-info').fadeIn('medium');
+    $curwindow = $('#track1-info');
+  });
+
+  $('#track2').on('click', () => {
+    $('.black-overlay').fadeIn('fast');
+    $('#track2-info').fadeIn('medium');
+    $curwindow = $('#track2-info');
+  });
+
+  $('.close').on('click', () => {
+    $('.black-overlay').fadeOut('fast');
+    $curwindow.fadeOut('fast');
+  });
+
+  $('#track3').on('click', () => {
+    $('.black-overlay').fadeIn('fast');
+    $('#track3-info').fadeIn('medium');
+    $curwindow = $('#track3-info');
+  });
+
+  $('.black-overlay').on('click', () => {
+    $('.black-overlay').fadeOut('fast');
+    $curwindow.fadeOut('fast');
+  });
+});
