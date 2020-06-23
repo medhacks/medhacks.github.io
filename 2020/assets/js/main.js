1f8096df80237f7b4c3c0fd1c189af932584a337
Alan Zhang <azhang42@jhu.edu>
2020-06-22 20:05:30 -0400

    Updated Team Bio Page and some fixees to 2020

--- 2020/assets/js/main.js
+++ 2020/assets/js/main.js
@@ -147,7 +147,6 @@ $(document).ready(function () {
   $("a").on("click", function (event) {
     if (this.hash !== "") {
       event.preventDefault();
-
       var hash = this.hash;
 
       $('html, body').animate({
--- team-bio/style/script.js
+++ team-bio/style/script.js
@@ -1,30 +1,15 @@
-// slide show JS
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 120
+      }, 1200, function () {
+      });
+    }
+  });
+});
