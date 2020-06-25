7abc329a177635b4bf50322c010782f90f1cca4c
Alan Zhang <azhang42@jhu.edu>
2020-06-24 21:13:45 -0400

    Fixed margins, fixed bug with resolutions

--- team-bio/style/script.js
+++ team-bio/style/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  console.log("Poggers");
   $("a").on("click", function (event) {
     if (this.hash !== "") {
       event.preventDefault();
@@ -17,26 +16,36 @@ $(document).ready(function () {
 var pics = document.getElementsByClassName("picture");
 var i;
 
-$(document).ready(function() {
-  if ($(window).width() < 830) {
-    for (i = 0; i < pics.length; i++) {
-      $(pics[i]).addClass('pictureMobile');
-    } 
-  } else {
-    for (i = 0; i < pics.length; i++) {
-      if (checkResolution(pics[i])) {
-        $(pics[i]).addClass('pictureTall');
-      } else {
-        $(pics[i]).addClass('pictureWide');
-      }
-    }
+for (i = 0; i < pics.length; i++) {
+  console.log(pics[i]);
+  let img = pics[i]
+  img.addEventListener('load', function() {
+    console.log(img);
+    img.onload = changeResolution(img);
+  });
+}
+
+
+window.onload = function() {
+  console.log("PogU");
+  for (i = 0; i < pics.length; i++) {
+    img = pics[i]
+    img.onload = changeResolution(img);
   }
-});
+} 
+
+function adjustResolution() {
+  for (i = 0; i < pics.length; i++) {
+    img = pics[i]
+    img.onload = changeResolution(img);
+  }
+}
 
 
 $(window).on('resize', function() {
   for (i = 0; i < pics.length; i++) {
-    changeResolution(pics[i]);
+    img = pics[i]
+    img.onload = changeResolution(img);
   }
 })
 
@@ -49,21 +58,11 @@ function changeResolution(n) {
     return;
   } else if (n.naturalHeight > n.naturalWidth) {
     $(n).addClass('pictureTall');
+    $(n).removeClass('pictureWide');
   } else {
     $(n).addClass('pictureWide');
+    $(n).removeClass('pictureTall');
   }
   $(n).removeClass('pictureMobile');
   return
 }
-
-var temp;
-function checkResolution(n) {
-  n.onload = function() {
-    if (n.naturalHeight > n.naturalWidth) {
-      temp = true;
-    } else {
-      temp = false;
-    } 
-  }
-  return temp;
-} 
