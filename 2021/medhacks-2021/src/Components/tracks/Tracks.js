bd364bf843a611a2b31b47afab5da1ce06053e94
Alan Zhang <azhang42@jhu.edu>
2021-04-23 23:25:05 -0400

    Update Tracks.js

--- 2021/medhacks-2021/src/Components/tracks/Tracks.js
+++ 2021/medhacks-2021/src/Components/tracks/Tracks.js
@@ -9,6 +9,7 @@ const Tracks = (props) => {
         <TrackBox
           titleNum={track.titleNum}
           title={track.title}
+          description={track.description}
           trackClick={props.trackClick}
           popup={props.popup}
         />
@@ -18,18 +19,20 @@ const Tracks = (props) => {
 };
 
 let tracks = [
-  { titleNum: "Track 1", title: "Aging in place with resilience and resouces" },
+  { 
+    titleNum: 1, 
+    title: "Aging in place with resilience and resouces",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut nunc ut massa tincidunt varius auctor vel ligula. Integer tempor tellus in ex pellentesque, a molestie ante bibendum. Sed vel purus sit amet justo ultrices tempor. Suspendisse potenti. Vivamus fermentum ante vulputate felis varius, vehicula luctus mauris scelerisque. Aliquam lacinia eros finibus ullamcorper lacinia. Morbi fringilla consequat aliquet. Cras eleifend scelerisque condimentum. Nullam dignissim sit amet arcu vitae tempus. Nam at venenatis libero. Donec tortor erat, condimentum eget magna ac, interdum efficitur mi. Pellentesque ante sem, volutpat et molestie non, condimentum a nisl. Aenean tincidunt suscipit posuere. Quisque non libero nunc. Nam bibendum diam metus, sed consequat felis posuere sit amet. Aliquam non mi vestibulum, placerat risus porta, placerat eros. Maecenas commodo, tellus quis lobortis semper, massa sapien imperdiet sem, eget hendrerit nunc metus a nulla. Praesent tristique tristique convallis. Vivamus vitae pretium tortor. Donec egestas diam suscipit arcu aliquet dignissim. Aenean auctor nulla sed vehicula accumsan. Maecenas porttitor tortor dolor, at consectetur urna tincidunt vitae. Etiam vitae ipsum porttitor, ullamcorper ipsum id, dictum orci. Pellentesque efficitur nisl eget magna mattis accumsan. Nam aliquet nunc a velit blandit fringilla. Sed magna est, ornare a tellus nec, eleifend feugiat magna. Nulla quis tempor magna. Morbi tellus risus, viverra a finibus sed, consequat vitae sapien.",
+  },
   {
-    titleNum: "Track 2",
+    titleNum: 2,
     title: "Personalized medicine using data driven healthcare",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at tellus sed mauris viverra iaculis vel sit amet risus. Mauris commodo iaculis erat vitae congue. Mauris sed sagittis dui. Sed sollicitudin rutrum maximus. Aliquam posuere lacinia erat eget iaculis. Cras vitae maximus est. Morbi tincidunt tortor ac dolor efficitur placerat. Praesent nisl risus, placerat a risus a, fermentum maximus purus. Nullam felis massa, consequat quis rutrum non, sollicitudin id metus. Vestibulum malesuada hendrerit velit vitae pharetra. In id congue metus. Phasellus erat lectus, malesuada ut dignissim vitae, elementum vitae urna. Nullam nec est at diam fringilla feugiat non in nisl. Vivamus ut nunc aliquet, rutrum urna quis, vestibulum justo. Pellentesque justo neque, convallis sed sapien ut, varius accumsan urna. Integer cursus finibus ligula, ac fringilla sapien gravida nec. Ut orci nulla, vehicula dictum blandit sit amet, pe"
   },
   {
-    titleNum: "Track 3",
+    titleNum: 3,
     title: "Patient adherence and quality care during a global pandemic",
-  },
-  {
-    titleNum: "Track 4",
-    title: "Patient adherence and quality care during a global pandemic",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate feugiat nisl nec mattis. Donec non lorem faucibus, blandit odio eu, lacinia dui. Donec rhoncus metus a lectus efficitur, a egestas leo pulvinar. Etiam aliquet ut tortor ac scelerisque. Mauris maximus tincidunt dui. Donec iaculis tellus ut volutpat convallis. Suspendisse potenti. Nulla ac nunc posuere, suscipit lorem a, pulvinar nulla. \n Nunc vel dapibus tortor. Maecenas auctor nunc ut est maximus egestas. Pellentesque vel sapien sit amet metus eleifend euismod ac sodales leo. Mauris ut dictum neque, sed porttitor justo. Nunc maximus tempor nunc. Phasellus semper congue arcu, in aliquam risus vestibulum quis. Cras blandit iaculis orci eget vulputate. Nunc a rutrum lorem. Donec eu tempus est."
   },
 ];
 
@@ -38,7 +41,12 @@ const parentStyle = {
   width: "60%",
   height: "100%",
   display: "flex",
+
   flexDirection: "row",
+
+  display: "-ms-flex",
+  display: "-webkit-flex",
+
 };
 
 export default Tracks;
