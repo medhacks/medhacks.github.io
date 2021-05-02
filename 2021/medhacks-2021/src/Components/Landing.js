4ac531abc8534c7e0edf42c8783ff0e9fc1652bb
Alan Zhang <azhang42@jhu.edu>
2021-05-02 00:36:32 -0400

    Updated Landing Page

--- 2021/medhacks-2021/src/Components/Landing.js
+++ 2021/medhacks-2021/src/Components/Landing.js
@@ -1,35 +1,43 @@
 import React from 'react';
 import { Link, BrowserRouter } from 'react-router-dom'
 
-import image from '../Images/landingImage.png'
+import pieces from '../Images/blurredpieces.png'
+import background from '../Images/gradient_wide.png'
+import text from '../Images/medhacks_text.png'
 import Main from './Main'
 
 const Landing = (Props) => {
-  const imgStyle = {
-    height: '100vh',
-    'margin-left': 'auto',
-    'margin-right': 'auto',
+  const backgroundStyle = {
     display: 'block',
+    zIndex: 0,
+    minHeight: '100%',
+    minWidth: '100%',
+    position: 'fixed',
+    top: 0,
   }
 
-  const buttonStyle = {
-    position: 'absolute',
-    height: '5%',
-    width: '20%',
-    top: '55%',
-    left: '45%',
-    color: 'white',
-    'font-size': '60px',
+  const piecesStyle = {
+    zIndex: 1,
+    position: 'fixed',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+  }
+
+  const textStyle = {
+    zIndex: 2,
+    position: 'fixed',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '120%',
   }
 
   return (
-    <div style={{overflow:'hidden', height:'100vh', width:'100%'}}>
-      <img style = {imgStyle} src={image} alt="Landing Image"></img>
-        <p style = {buttonStyle} >
-          <Link to="/2021">
-            apply now
-          </Link>
-        </p>
+    <div style={{overflow:'hidden', height:'100%', width:'100%'}}>
+      <img style = {backgroundStyle} src={background} alt="Landing Page Background"></img>
+      <img style = {piecesStyle} src={pieces} alt="Landing Page Background"></img>
+      <img style = {textStyle} src={text} alt="Landing Page Background"></img>
     </div>
   )
 }
