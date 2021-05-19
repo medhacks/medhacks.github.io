d01f3ade8aa4c269f43909022c53eed070e5dd4f
MattLiu-mygit <mliu78@jh.edu>
2021-05-19 17:14:32 -0400

    landing logo updates

--- 2021/medhacks-2021/src/Components/Landing/Landing.js
+++ 2021/medhacks-2021/src/Components/Landing/Landing.js
@@ -1,61 +1,57 @@
 import React from "react";
-import logo from '../../Images/mh logo filled.png'
-import pieces from '../../Images/adjustedfade.png'
-import logoText from '../../Images/mh-text.png'
+import logo from "../../Images/mh logo filled.png";
+import pieces from "../../Images/adjustedfade.png";
+import logoText from "../../Images/mh-text.png";
 
 const Landing = () => {
-
   const logoContainerStyle = {
     zIndex: 2,
-    height: '30vh',
-    //width: '60vw',
-    //minWidth: '300px',
-    margin: '0',
-    position: 'absolute',
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
-    display: 'flex',
-    justifyContent: 'center',
-    //width: 'auto',
-  }
+    height: "30vh",
+    width: "60vw",
+    margin: "0",
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+    display: "flex",
+  };
 
   const logoStyle = {
-    position: 'relative',
-    maxHeight: '100%',
+    maxHeight: "20vw",
     filter: "drop-shadow(7px 7px 7px rgb(0, 0, 0, 0.75))",
-    float: 'left',
-    display: 'inline-block',
-    //paddingLeft: '10px', 
-    //maxWidth: '20%',
-  }
+    maxWidth: "20vw",
+  };
   const logoTextStyle = {
-    position: 'relative',
-    display: 'inline-block',
-    float: 'left',
     filter: "drop-shadow(7px 7px 7px rgb(0, 0, 0, 0.75))",
-    transform: 'translate(-15%, 0)',
-    height: '30%'
-  }
+    transform: "translate(-15%, 0)",
+    maxHeight: "7vw",
+    maxWidth: "45vw",
+    flexShrink: 1,
+  };
 
   const piecesStyle = {
     zIndex: 1,
-    position: 'absolute',
-    left: '50%',
-    top: '50%',
-    maxWidth: '80%',
-    transform: 'translate(-50%, -50%)',
-  }
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    maxWidth: "80%",
+    transform: "translate(-50%, -50%)",
+    flexShrink: 1,
+  };
 
   return (
-    <div style = {{ "height": "100vh", "overflow-x": "hidden"}}>
-      <img style = {piecesStyle} src={pieces} alt="Landing Page Background"></img>
+    <div style={{ height: "100vh", "overflow-x": "hidden" }}>
+      <img style={piecesStyle} src={pieces} alt="Landing Page Background"></img>
       <div style={logoContainerStyle}>
-          <img style={logoStyle} src={logo} alt="Landing Page Background" />
-          <img style={logoTextStyle} src={logoText} alt="Landing Page Background" />
+        <img style={logoStyle} src={logo} alt="Landing Page Background" />
+        <img
+          style={logoTextStyle}
+          src={logoText}
+          alt="Landing Page Background"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Landing;
