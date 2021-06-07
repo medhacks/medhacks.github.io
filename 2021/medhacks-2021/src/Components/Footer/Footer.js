a51efa16b1fc808e251a289ede3048a89c4804b6
Alan Zhang <azhang42@jhu.edu>
2021-06-07 16:48:44 -0400

    Updated Footer, Sponsors

--- 2021/medhacks-2021/src/Components/Footer/Footer.js
+++ 2021/medhacks-2021/src/Components/Footer/Footer.js
@@ -2,56 +2,68 @@ import React from "react";
 import { HashLink as Link } from 'react-router-hash-link'
 import { ReactComponent as Instagram } from '../../Images/instagram.svg'
 import { ReactComponent as Facebook } from '../../Images/facebook.svg'
+import Pieces from '../../Images/footer_2.png'
 
 const Footer = () => {
 
   const container = {
-    backgroundColor: '#a992ee',
-    background: 'linear-gradient(0deg, #a992ee 0%, #82b4e6 46%, #b0edd6 100%)',
+    paddingTop: '2%',
+    //backgroundColor: '#a992ee',
     display: 'inline-block',
     width: '100%',
+    backgroundImage: 'url(' + Pieces+ ')',
+    //backgroundImage: 'url(' + Pieces+ '), linear-gradient(0deg, #a992ee 0%, #82b4e6 46%, #b0edd6 100%)',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center bottom',
+    backgroundSize: '100%'
   }
 
   const link = {
     color: 'white',
     marginBottom: '1%',
     fontSize: '1.2em',
+    float: 'right',
   }
 
-  const imgLink = {
-    width: '5%',
-    height: '5%',
-    marginLeft: '1%', 
+  const whiteContainer = {
+    backgroundColor: 'rgb(100, 100, 100, .4)',
+    boxShadow: "0px 0px 50px rgba(100,100,100,.5)",
+    borderRadius: '10px',
+    width: '80%',
+    marginLeft: '10%',
+    marginBottom: '5%',
   }
 
   return(
     <div style={container}>
-      <div className="footer-text">
-        <p>
-          MedHacks brings together diverse minds from all backgrounds to foster collaboration towards solving the world's most pressing medical issues.
-        </p>
-      </div>
-      <div className="footer-links">
-        <h2>quick links</h2>
-        <Link style={link} to={"/2021#"} >
-          home
-        </Link>
-        <Link style={link} to={{pathname:"https://www.google.com"}} target='blank'>
-          apply
-        </Link>
-        <Link style={link} to={{pathname:"mailto: info@medhacks.io"}} target='blank'>
-          email us
-        </Link>
-        <Link style={link} to={{pathname:"https://docs.google.com/document/d/1VEFstsX7jg9W7uJSyx3ty_wcU-8yiXRlpP7MAelIIHA/"}} target='blank'>
-          Code of Conduct
-        </Link>
-        <div>
-          <Link to={{pathname:"https://www.instagram.com/medhacks/"}} target='blank'>
-            <Instagram className="footer-img" />
+      <div style={whiteContainer}>
+        <div className="footer-text">
+          <p>
+            MedHacks brings together diverse minds from all backgrounds to foster collaboration towards solving the world's most pressing medical issues.
+          </p>
+        </div>
+        <div className="footer-links">
+          <h2>quick links</h2>
+          <Link style={link} to={"/2021#"} >
+            home
           </Link>
-          <Link to={{pathname:"https://www.facebook.com/medhacks"}} target='blank'>
-            <Facebook className="footer-img" />
+          <Link style={link} to={{pathname:"https://www.google.com"}} target='blank'>
+            apply
           </Link>
+          <Link style={link} to={{pathname:"mailto: info@medhacks.io"}} target='blank'>
+            email us
+          </Link>
+          <Link style={link} to={{pathname:"https://docs.google.com/document/d/1VEFstsX7jg9W7uJSyx3ty_wcU-8yiXRlpP7MAelIIHA/"}} target='blank'>
+            Code of Conduct
+          </Link>
+          <div>
+            <Link to={{pathname:"https://www.instagram.com/medhacks/"}} target='blank'>
+              <Instagram className="footer-img" />
+            </Link>
+            <Link to={{pathname:"https://www.facebook.com/medhacks"}} target='blank'>
+              <Facebook className="footer-img" />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- 2021/medhacks-2021/src/Components/Sponsors/Sponsors.js
+++ 2021/medhacks-2021/src/Components/Sponsors/Sponsors.js
@@ -2,13 +2,33 @@ import React from "react";
 
 const Sponsors = (props) => {
   const sponsorStyle = {
-    width: "33%",
-    display: "inline-block",
+    width: "100%",
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'block',
   }
 
   const parentStyle = {
     margin: "auto",
     width: "60%",
+    backgroundColor: 'rgb(255, 255, 255, .6)',
+    disply: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  }
+
+  const primary = {
+    //width: '50%',
+    flexBasis: '50%',
+    display: 'inline-block',
+    height: '100%',
+  }
+
+  const secondary = {
+    flexBasis: '25%',
+    //width: '25%',
+    display: 'inline-block',
+    height: '100%',
   }
 
   return (
@@ -16,12 +36,17 @@ const Sponsors = (props) => {
       <h1 style={{textAlign:"center"}}>2020 Partners</h1>
 
       <div style = {parentStyle}>
-          <img style={sponsorStyle} src="https://d2xssgyjy4ywy8.cloudfront.net/2020-logos/BME.png"
-                  alt="JHU Biomedical Engineering"></img>
-          <img style={sponsorStyle} src="https://d2xssgyjy4ywy8.cloudfront.net/2020-logos/Contrary.png"
-                      alt="Contrary Capital" />
-          <img style={sponsorStyle} src="https://d2xssgyjy4ywy8.cloudfront.net/2020-logos/GoogleCloud.png"
-                      alt="Google Cloud" />
+        <div style={secondary}>
+          <img style={sponsorStyle} src="https://d2xssgyjy4ywy8.cloudfront.net/2020-logos/BME.png" alt="JHU Biomedical Engineering"></img>
+          <img style={sponsorStyle} src="https://d2xssgyjy4ywy8.cloudfront.net/2020-logos/BME.png" alt="JHU Biomedical Engineering"></img>
+        </div>
+        <div style={primary}>
+          <img style={sponsorStyle} src="https://d2xssgyjy4ywy8.cloudfront.net/2020-logos/Contrary.png" alt="Contrary Capital" />
+        </div>
+        <div style={secondary}>
+          <img style={sponsorStyle} src="https://d2xssgyjy4ywy8.cloudfront.net/2020-logos/GoogleCloud.png" alt="Google Cloud" />
+          <img style={sponsorStyle} src="https://d2xssgyjy4ywy8.cloudfront.net/2020-logos/GoogleCloud.png" alt="Google Cloud" />
+        </div>
       </div>
     </div>
   )
