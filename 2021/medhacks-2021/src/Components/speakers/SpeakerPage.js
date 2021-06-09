b8e32f24be9e4b96470a81bbcc502d63fb06597a
MattLiu-mygit <mliu78@jh.edu>
2021-06-09 18:20:51 -0400

    Speaker areas updates to carousel/speaker page

--- 2021/medhacks-2021/src/Components/speakers/SpeakerPage.js
+++ 2021/medhacks-2021/src/Components/speakers/SpeakerPage.js
@@ -6,6 +6,8 @@ import SpeakerArea from "./SpeakerArea";
 import Carousel from "./Carousel";
 import { useLocation } from "react-router-dom";
 import SpeakerList from "./SpeakerList";
+import Piece5 from "../../Images/puzzlePieces/5.png";
+import Piece6 from "../../Images/puzzlePieces/6.png";
 
 const SpeakerPage = () => {
   const [index, setIndex] = useState(0);
@@ -13,20 +15,39 @@ const SpeakerPage = () => {
 
   return (
     <>
-      <div
-        style={{
-          fontSize: "2.5rem",
-          textAlign: "center",
-          marginBottom: "2.5rem",
-          marginTop: "2.5rem",
-        }}
-      >
-        <h1 className="section-title">speakers</h1>
-      </div>
       {location.pathname === "/speakers" ? (
-        <SpeakerList />
+        <>
+          <div
+            style={{
+              fontSize: "2.5rem",
+              textAlign: "center",
+              marginBottom: "2.5rem",
+              marginTop: "2.5rem",
+            }}
+          >
+            <h1 className="section-title">speakers</h1>
+          </div>
+          <SpeakerList />
+        </>
       ) : (
-        <div style={{ marginTop: "2.5rem", height: window.innerHeight * 0.45 }}>
+        <div
+          id="tracks"
+          className="section-container"
+          style={{
+            backgroundColor: "rgb(17, 61, 86, .2)",
+            marginTop: "2.5rem",
+            height: "30vw",
+            // position: "relative",
+          }}
+        >
+          <div
+            className="section-title-container"
+            style={{ marginBottom: "1.5rem" }}
+          >
+            <img src={Piece5} alt="p5"></img>
+            <h1 className="section-title">speakers</h1>
+            <img src={Piece6} alt="p6"></img>
+          </div>
           <Carousel items={speakers} active={0} setIndex={setIndex} />
         </div>
       )}
