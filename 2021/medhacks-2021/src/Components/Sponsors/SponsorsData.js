3b0489cc9f431c7b0f95978c5e9724be44997ffb
Alan Zhang <azhang42@jhu.edu>
2021-06-11 21:55:11 -0400

    Updated Links to sponsors

--- 2021/medhacks-2021/src/Components/Sponsors/SponsorsData.js
+++ 2021/medhacks-2021/src/Components/Sponsors/SponsorsData.js
@@ -55,48 +55,48 @@ export const sponsorsData = [
   <>
     <div style = {parentStyle}>
         <div style={secondary}>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={newInnovations} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://www.new-innov.com/pub/"}} target='blank'>
+            <img style={sponsorStyle} src={newInnovations} alt="New Innovations"></img>
           </Link>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={malone} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://malonecenter.jhu.edu/"}} target='blank'>
+            <img style={sponsorStyle} src={malone} alt="The Malone Center for Engineering in Healthcare"></img>
           </Link>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={cloud} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://cloud.google.com/"}} target='blank'>
+            <img style={sponsorStyle} src={cloud} alt="Google Cloud"></img>
           </Link>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={idies} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://idies.jhu.edu/"}} target='blank'>
+            <img style={sponsorStyle} src={idies} alt="The Institute for Data Intensive Engineering and Science"></img>
           </Link>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={echoAR} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://www.echoar.xyz/"}} target='blank'>
+            <img style={sponsorStyle} src={echoAR} alt="echoAR"></img>
           </Link>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={ffu} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://ventures.jhu.edu/programs-services/fastforward-u/"}} target='blank'>
+            <img style={sponsorStyle} src={ffu} alt="Fast Forward U"></img>
           </Link>
         </div>
         <div style={primary}>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={cbid} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://cbid.bme.jhu.edu/"}} target='blank'>
+            <img style={sponsorStyle} src={cbid} alt="Center for Bioengineering Innovation and Design"></img>
           </Link>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-          <img style={sponsorStyle} src={careFirst} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://www.carefirst.com/"}} target='blank'>
+          <img style={sponsorStyle} src={careFirst} alt="CareFirst BlueCross BlueShield"></img>
           </Link>
         </div>
         <div style={secondary}>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={healthx} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://www.healthxventures.com/"}} target='blank'>
+            <img style={sponsorStyle} src={healthx} alt="HealthX Ventures "></img>
           </Link>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={nursing} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://nursing.jhu.edu/"}} target='blank'>
+            <img style={sponsorStyle} src={nursing} alt="School of Nursing at Johns Hopkins University"></img>
           </Link>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={tedco} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://www.tedcomd.com/"}} target='blank'>
+            <img style={sponsorStyle} src={tedco} alt="Maryland Technology Development Corporation"></img>
           </Link>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={mathWorks} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://www.mathworks.com/"}} target='blank'>
+            <img style={sponsorStyle} src={mathWorks} alt="MathWorks"></img>
           </Link>
-          <Link to={{ pathname: "https://www.google.com"}} target='blank'>
-            <img style={sponsorStyle} src={infinite} alt="JHU Biomedical Engineering"></img>
+          <Link to={{ pathname: "https://www.i-biomed.com/"}} target='blank'>
+            <img style={sponsorStyle} src={infinite} alt="Infinite Biomedical Tech"></img>
           </Link>
           
         </div>
