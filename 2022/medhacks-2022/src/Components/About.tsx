1138822d00881c5b30fde64f9825535289bbdd60
GWCMelody <melody2011102@gmail.com>
2022-06-14 17:42:17 -0400

    Setup app

---
+++ 2022/medhacks-2022/src/Components/About.tsx
@@ -0,0 +1,7 @@
+export default function Main() {
+    return (
+        <div>
+            <p style={{fontFamily:"Sancreek"}}>Add things here :)</p>
+        </div>
+    );
+}
---
+++ 2022/medhacks-2022/src/Components/Main.tsx
@@ -0,0 +1,9 @@
+import About from "./About";
+
+export default function Main() {
+    return (
+        <div>
+            <About/>
+        </div>
+    );
+}
