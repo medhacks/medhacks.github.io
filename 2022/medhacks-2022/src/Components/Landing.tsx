50a113c9292bfe2f9bb43696e6c310f007b0f586
Matthew Liu <mliu78@jh.edu>
2022-06-15 03:50:01 -0700

    feat: Added typing

--- 2022/medhacks-2022/src/Components/Landing.tsx
+++ 2022/medhacks-2022/src/Components/Landing.tsx
@@ -2,7 +2,12 @@ import { ReactComponent as PlaneSVG } from '../assets/plane.svg';
 import { ReactComponent as LogoSVG } from '../assets/logo.svg';
 import bg from '../assets/background.png';
 import map from '../assets/map.jpg';
-const options = [
+import { FC } from 'react';
+
+export type Section = 'Home' | 'About' | 'Tracks' | 'Apply' | 'Logistics';
+type HeaderOptionType = { label: Section; value: string };
+
+const options: HeaderOptionType[] = [
   { label: 'Home', value: '/home' },
   { label: 'About', value: '/about' },
   { label: 'Tracks', value: '/tracks' },
@@ -10,14 +15,14 @@ const options = [
   { label: 'Logistics', value: '/logistics' },
 ];
 
-const Landing = () => {
+const Landing: FC = () => {
   return (
     <div className="w-full h-[screen] bg-green-600">
       <div className="bg-white py-2 px-4 w-full flex flex-row">
         <div className="grow">
           <LogoSVG className="cursor-pointer w-[25%] h-[100%] my-auto ml-5" />
         </div>
-        {options.map((option) => (
+        {options.map((option: HeaderOptionType) => (
           <button className="flex flex-col py-1 px-2 m-2  transition ease-in-out delay-150 hover:scale-110 duration-300 rounded hover:bg-red-100">
             <div className="my-auto text-2xl mx-auto">{option.label}</div>
             <div className="my-auto mx-auto">{option.value}</div>
