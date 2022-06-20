57507f1194ca414d4f1bc27b9413d8ea1daae54f
will tong <46694175+willtong77@users.noreply.github.com>
2022-06-20 13:35:31 -0400

    added section for speakers

--- 2022/medhacks-2022/src/Components/Main.tsx
+++ 2022/medhacks-2022/src/Components/Main.tsx
@@ -1,11 +1,13 @@
 import About from './About';
 import Landing from './Landing';
+import Speakers from './Speakers';
 
 export default function Main() {
   return (
     <div>
       <Landing />
       <About />
+      <Speakers />
     </div>
   );
 }
