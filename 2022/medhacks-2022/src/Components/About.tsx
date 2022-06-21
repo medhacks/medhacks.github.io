0ba2813184e3e0ce08432f85f9e73c67a46a6e02
Allen Shen <allenshen1112@gmail.com>
2022-06-20 20:57:40 -0400

    about page

--- 2022/medhacks-2022/src/Components/About.tsx
+++ 2022/medhacks-2022/src/Components/About.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
-import { ReactComponent as AboutSVG } from '../assets/aboutheader.svg';
-
+import { ReactComponent as AboutSVG } from '../assets/About_Header_SVG.svg';
+import { ReactComponent as AboutheaderSVG } from '../assets/aboutheader.svg';
 
 const About: FC = () => {
   return (
