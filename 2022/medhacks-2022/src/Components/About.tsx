28ca83d8c407c862fc50ceb235906088d0751a92
Allen Shen <allenshen1112@gmail.com>
2022-06-20 20:14:51 -0400

    about page

--- 2022/medhacks-2022/src/Components/About.tsx
+++ 2022/medhacks-2022/src/Components/About.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import { FC } from 'react';
+import { ReactComponent as AboutSVG } from '../assets/aboutheader.svg';
 
-const About = () => {
+
+const About: FC = () => {
   return (
-    <div>
-      <p className="font-[Sancreek]">Add things heree :)</p>
-    </div>
-  );
+      <div className="w-full bg-[#8FC150] flex flex-col">
+          <div className="w-full flex flex-row justify-center items-center h-64 hover:scale-[102%] transition ease-in-out delay-50">
+              <AboutSVG className="scale-[50%]" />
+          </div>
+          <div className="px-80 text-white text-xl text-justify font-['Mulish'] leading 7">
+              <p className="">It was started in 2015 by undergraduate students at Johns Hopkins
+                  University with the mission to bring together talented and diverse
+                  minds from all backgrounds and to foster collaboration towards
+                  solving the world's most pressing medical issues. As a result,
+                  MedHacks has hosted hackers with diverse majors ranging from public
+                  health to anthropology to computer science. Over the years, MedHacks
+                  has been attended by thousands from over 500 institutions and 30
+                  countries, has formed partnerships with companies at the forefront
+                  of medicine and technology including Blue Cross Blue Shield, CVS
+                  Health, Google, and MathWorks, and has established itself as one of
+                  the world's most recognized and influential medical hackathons.
+              </p>
+              <p className="font-bold">We can't wait to see you and your ideas at MedHacks 2022!</p>
+          </div>
+      </div>
+
+  )
 };
 
 export default About;
