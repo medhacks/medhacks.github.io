1ff82d6b1e3fa12eb9a9911840bd35530ed462f8
Allen Shen <allenshen1112@gmail.com>
2022-08-02 17:23:03 -0400

    added a logo to Sponsors.tsx

--- 2022/medhacks-2022/src/Components/Sponsors.tsx
+++ 2022/medhacks-2022/src/Components/Sponsors.tsx
@@ -8,10 +8,15 @@ const Sponsors: FC = () => {
       <div className="w-full flex flex-row justify-center items-center h-64 hover:scale-[102%] transition ease-in-out delay-50">
         <SponsorsSVG className="scale-[70%]" />
       </div>
-      <div className="px-[10%] text-white text-xl text-justify font-['Mulish'] leading 7">
-      </div>
-        <div className="flex flex-row">
-            <div className="basis-1/4">
+      <div className="px-[10%] text-white text-xl text-justify font-['Mulish'] leading 7"></div>
+        <div className="flex  items-center gap-16">
+            <div className="basis-1/3 ml-2 mr2 object-cover">
+                <img src={bourne} alt="Bourne Logo" />
+            </div>
+            <div className="basis-1/3 ml-2 mr2 object-cover">
+                <img src={amplitude} alt="amplitude Logo" />
+            </div>
+            <div className="basis-1/3 ml-2 mr8 object-cover">
                 <img src={bourne} alt="Bourne Logo" />
             </div>
         </div>
