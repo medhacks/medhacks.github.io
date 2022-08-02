0f7c4c11537810a6826ce3d854ec8ccd45953567
Allen Shen <allenshen1112@gmail.com>
2022-08-02 13:55:29 -0400

    added a logo to Sponsors.tsx

--- 2022/medhacks-2022/src/Components/Sponsors.tsx
+++ 2022/medhacks-2022/src/Components/Sponsors.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { ReactComponent as SponsorsSVG } from '../assets/sponsors_header.svg';
-
+import bourne from '../assets/bourne digital.png'
+import amplitude from '../assets/amplitude.png'
 const Sponsors: FC = () => {
   return (
     <div className="w-full flex flex-col ">
@@ -8,8 +9,12 @@ const Sponsors: FC = () => {
         <SponsorsSVG className="scale-[50%]" />
       </div>
       <div className="px-[10%] text-white text-xl text-justify font-['Mulish'] leading 7">
-        
       </div>
+        <div className="flex flex-row">
+            <div className="basis-1/4">
+                <img src={bourne} alt="Bourne Logo" />
+            </div>
+        </div>
     </div>
   );
 };
