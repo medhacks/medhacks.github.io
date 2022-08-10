6966a104bbe16a5713301abfe3d6e70698f44bf0
melodyhplee <lee.hsinpei.melody@gmail.com>
2022-08-10 07:56:26 -0400

    for gh pages deployment

--- 2021/medhacks-2021/src/Components/speakers/speakerData/speakers.js
+++
@@ -1,8 +0,0 @@
-import aChi from "./AlbertChi/data";
-import eDong from "./EnshengDong/data";
-
-const speakers = [
-  { ...aChi, image: "https://mmedhacks-2021-website.s3.amazonaws.com/Speakers/AlbertChi.png" },
-  { ...eDong, image: "https://mmedhacks-2021-website.s3.amazonaws.com/Speakers/EnshengDong.png" },
-];
-export default speakers;
--- 2022/medhacks-2022/src/Components/Spacer.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-const Spacer: FC<{
-  sectionId: string;
-}> = ({ sectionId }) => {
-  return <div id={sectionId} className="h-20"></div>;
-};
-
-export default Spacer;
