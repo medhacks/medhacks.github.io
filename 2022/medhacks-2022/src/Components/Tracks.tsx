ef6a4be927f440d314dfdad3e62ca0aab2decbd0
will tong <46694175+willtong77@users.noreply.github.com>
2022-08-27 19:35:01 -0400

    updated tracks

--- 2022/medhacks-2022/src/Components/Tracks.tsx
+++ 2022/medhacks-2022/src/Components/Tracks.tsx
@@ -1,18 +1,27 @@
 import { FC } from 'react';
 import { ReactComponent as TracksHeaderSVG } from '../assets/tracks_header.svg';
+import APL from '../assets/apl.png';
 
 const TrackCard: FC<{
   trackName: string;
   description: string;
-}> = ({ trackName, description }) => {
+  img: string;
+}> = ({ trackName, description, img = "" }) => {
   return (
-    <div className="flex justify-center p-6 h-72 max-w-xs">
+    <div className="flex justify-center p-6 max-w-lg max-h-lg">
       <div className="rounded-2xl bg-[#FCE689] flex justify-center p-4 box-border ">
         <div className="border-4 hover:border-rose-600 rounded-2xl border-white">
-          <h5 className="text-gray-900 text-xl text-center leading-tight font-medium font-bold font-sans p-3">
-            {trackName}
-          </h5>
-          <p className="text-gray-700 text-center text-sm font-sans p-2">
+          <div className="">
+            <h5 className="text-gray-900 text-xl text-center leading-tight font-medium font-extrabold font-sans p-2">
+              {trackName}
+            </h5>
+            {
+              img === ""
+              ? null
+              : <img className="max-h-xs" src={img} alt="Tracks"/>
+            }
+          </div>
+          <p className="text-gray-700 text-center text-md font-sans pt-2 pb-3 px-2">
             {description}
           </p>
         </div>
@@ -26,29 +35,23 @@ const Tracks: FC = () => {
     <div className="w-full flex flex-col">
       {/* Tracks Header */}
       <div className="w-full flex flex-row justify-center items-center h-64 hover:scale-[102%] transition ease-in-out delay-50 z-20">
-        <TracksHeaderSVG className="w-[40%] min-w-[300px] max-w-[500px] h-auto"  />
+        <TracksHeaderSVG className="w-[40%] min-w-[300px] max-w-[500px] h-auto" />
       </div>
 
       {/* Description of Tracks */}
       <p className="font-sans text-white md:text-2xl sm:text-lg pl-[2%] pr-[2%] bg-[#CFACB9] mr-[5%] ml-[5%] pt-10 pb-10 rounded-3xl">
-        Tracks are coming soon! This year's tracks will be coming from JHU SoN and APL. Stay tuned for more information on tracks and prizes!
+        MedHacks and its sponsors create three tracks every year.
       </p>
 
       {/* Track Cards */}
-      {/* <div className="flex flex-row justify-center flex-wrap">
+      <div className="flex flex-row justify-center flex-wrap">
+        {/* JHU APL - Health and Data Science Innovation Track */}
         <TrackCard
-          trackName="Track 1"
-          description="When cursor hovers over a track, border turns red. Centered description"
+          trackName="Health and Data Science Innovation Track"
+          description="Your team's mission is to help us advance patient care through an open science “hack.” The possibilities are endless. Identify an emerging issue related to patient care, and then come up with creative and unique solutions to reduce or resolve them. Your project may build on the following focus areas: scaling technology platforms, moving treatment technology from the hospital to the home, improving hospital operation or medical supply logistics, making data actionable, and empowering patients."
+          img={APL}
         />
-        <TrackCard
-          trackName="Track 2"
-          description="This is just a simple description"
-        />
-        <TrackCard
-          trackName="Track 3"
-          description="This is just a simple description"
-        />
-      </div> */}
+      </div>
     </div>
   );
 };
