af0d7f499db0ac77ea07cd79ac23351f45e394bc
melodyhplee <lee.hsinpei.melody@gmail.com>
2022-08-29 16:00:52 -0400

    adjusted footer

--- 2022/medhacks-2022/src/Components/Footer.tsx
+++ 2022/medhacks-2022/src/Components/Footer.tsx
@@ -4,8 +4,8 @@ import { ReactComponent as LogoSVG } from '../assets/logo_plain.svg';
 import footerLogo from '../assets/medhacks_logo.png'
 const Footer: FC = () => {
   return (
-    <div className="flex z-20 h-[30vh]">
-      <div className="h-auto">
+    <div className="flex z-20 w-[90%] justify-center h-auto m-auto pb-[20px]">
+      <div>
           <img src={footerLogo} alt="Medhacks Logo" />
       </div>
     </div>
