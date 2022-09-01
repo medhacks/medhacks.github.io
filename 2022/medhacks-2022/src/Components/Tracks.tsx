14eb03fcd64b4559450d1e4df481fbdf0098d18b
melodyhplee <lee.hsinpei.melody@gmail.com>
2022-09-01 18:59:14 -0400

    added third track

--- 2022/medhacks-2022/src/Components/Tracks.tsx
+++ 2022/medhacks-2022/src/Components/Tracks.tsx
@@ -2,10 +2,11 @@ import { FC } from 'react';
 import { ReactComponent as TracksHeaderSVG } from '../assets/tracks_header.svg';
 import APL from '../assets/apl.png';
 import CILDI from '../assets/CILDI_White Vertical.png';
+import PSTC from '../assets/PSTC logo.png';
 
 const TrackCard: FC<{
   trackName: string;
-  description: string;
+  description: React.ReactElement<any>;
   img: string;
 }> = ({ trackName, description, img = "" }) => {
   return (
@@ -19,12 +20,12 @@ const TrackCard: FC<{
             {
               img === ""
                 ? null
-                : <img className="max-h-xs" src={img} alt="Tracks" />
+                : <img className="max-h-xs m-auto" src={img} alt="Tracks" />
             }
           </div>
-          <p className="text-gray-700 text-center text-md font-sans pt-10 pb-3 px-2">
+          <div className="text-gray-700 text-center text-md font-sans pt-10 pb-3 px-2">
             {description}
-          </p>
+          </div>
         </div>
       </div>
     </div>
@@ -41,7 +42,7 @@ const Tracks: FC = () => {
 
       {/* Description of Tracks */}
       <p className="font-sans text-black md:text-2xl sm:text-lg pl-[2%] pr-[2%]  mr-[5%] ml-[5%] pt-10 pb-10 rounded-3xl">
-        MedHacks and their sponsors are pleased to present two title tracks this year.
+        MedHacks and their sponsors are pleased to present three title tracks this year!
       </p>
 
       {/* Track Cards */}
@@ -49,14 +50,30 @@ const Tracks: FC = () => {
         {/* JHU APL - Health and Data Science Innovation Track */}
         <TrackCard
           trackName="Health and Data Science Innovation Track"
-          description="Your team's mission is to help us advance patient care through an open science “hack.” The possibilities are endless. Identify an emerging issue related to patient care, and then come up with creative and unique solutions to reduce or resolve them. Your project may build on the following focus areas: scaling technology platforms, moving treatment technology from the hospital to the home, improving hospital operation or medical supply logistics, making data actionable, and empowering patients."
+          description={<div>Your team's mission is to help us advance patient care through an open science “hack.” The possibilities are endless. Identify an emerging issue related to patient care, and then come up with creative and unique solutions to reduce or resolve them. Your project may build on the following focus areas: scaling technology platforms, moving treatment technology from the hospital to the home, improving hospital operation or medical supply logistics, making data actionable, and empowering patients.</div>}
           img={APL}
         />
         {/* School of Nursing Track */}
         <TrackCard
-          trackName="Innovations in reducing human errors and improving patient safety in hospital settings"
-          description=""
+          trackName="Innovations in Reducing Human Errors and Improving Patient Safety in Hospital Settings"
+          description={<div></div>}
           img={CILDI}
+        />
+        {/* BIG IDEA: Best Technology-Enabled Patient Safety Solutions Track */}
+        <TrackCard
+          trackName="BIG IDEA: Best Technology-Enabled Patient Safety Solutions"
+          description={
+            <div><p>We’re in search of bold new thinking. This is a call to the NEXT GENERATION. An invitation to solve the problem of medical error that harms millions of U.S. patients and costs billions of dollars every year. We’re calling on MedHacks teams to envision the best technology-enabled patient safety solution that has the potential to avert patient harm and save lives. Your hack must align with one of the following four leading patient safety challenges facing health care across the continuum of care.
+              </p><br/>
+              <ol type="1" className="text-left pl-5">
+                <li> * Procedural/surgical errors (e.g. pneumothorax, excessive bleeding, intestinal perforation)</li>
+                <li>  * Errors during routine patient care (e.g. pressure ulcers, blood clots, falls)</li>
+                <li>  * Infections (e.g. respiratory infections, surgical site infections, central line-associated blood stream infections)</li>
+                <li>  * Diagnostic safety (e.g. missed, delayed or wrong diagnoses)</li>
+              </ol><br/>
+              <p>We encourage teams to consider autonomous patient safety solutions, those where the automation of an existing technology can improve patient safety by easing the burden on our healthcare workforce that is burning out and turning over at astonishing rates that are not sustainable.</p></div>
+          }
+          img={PSTC}
         />
       </div>
     </div>
